fd48c197f6023540a58c43c4a0439664ea59354d
Minseok Jeon <cheon7886@naver.com>
2017-05-07 00:34:10 +0900

    Final Rollout

---
+++ album/common.js
@@ -0,0 +1,141 @@
+var imageCount = nameArray.length-1;
+
+linkArray[-1] = '/?action=PluginResource&type=album&type=album_no_left.png';
+nameArray[-1] = 'no image';
+
+if(imageCount == 0){
+	imageCount = 1;
+	linkArray[0] = '/?action=PluginResource&type=album&type=album_no_image.png';
+	nameArray[0] = 'NO IMAGE';
+}
+
+linkArray[imageCount] = '/?action=PluginResource&type=album&type=album_no_right.png';
+nameArray[imageCount] = 'no image';
+
+function onClickListItem(i){
+	currentImage = i;
+	Redraw();
+}
+
+hashpage = Number(window.location.hash.substr(1));
+if(hashpage != currentImage) {
+	currentImage = hashpage;
+}
+
+function onClickNext(){
+	currentImage++;
+	if(currentImage >= imageCount) currentImage = imageCount - 1;
+	Redraw();
+}
+
+function onClickPrev(){
+	currentImage--;
+	if(currentImage < 0) currentImage = 0;
+	Redraw();
+}
+
+//var cachedImage = new Image();
+
+function Redraw(){
+	var prevIdx = currentImage - 1;
+	var nextIdx = currentImage + 1;
+	var imgClass = '';
+	if (zoomRatio != 'real') { imgClass = ' '+zoomRatio; }
+	//cachedImage.src = linkArray[nextIdx];
+
+	var imagePane = document.getElementById('imagePane');
+	imagePane.innerHTML = '<a href="#'+nextIdx+'"><img id="albumimg" class="main'+imgClass+'" src="'+linkArray[currentImage]+'" /></a>';
+
+	var filenamePane = document.getElementById('filenamePane');
+	filenamePane.innerHTML = nameArray[currentImage] + " &nbsp; (" + (currentImage) + "/" + Number(imageCount-1) + ")";
+
+	// 좌우 이동 버튼
+	if(currentImage > 0) { // 이전 페이지로 이동 가능
+		document.getElementById('button_left_on').style.display = 'block';
+		document.getElementById('control_prev').href = "#" + (prevIdx + 1);
+	}
+	else { // 이전 페이지로 이동 불가능
+		document.getElementById('button_left_on').style.display = 'none';
+	}
+	if(currentImage < imageCount-1) { // 다음 페이지로 이동 가능
+		document.getElementById('button_right_on').style.display = 'block';
+		document.getElementById('control_next').href = "#" + (nextIdx - 1);
+	}
+	else { // 다음 페이지로 이동 불가능
+		document.getElementById('button_right_on').style.display = 'none';
+	}
+	
+	RedrawList();
+	scroll(0, 0);
+}
+
+function ScrollToon(){
+    var ImageBuf = '';
+    for(var j=0; j<imageCount; j++){
+        ImageBuf += '<img id="scrollimg_'+j+'" class="Scrolling" src="'+linkArray[j]+'">\n';
+    }
+    var ScrollList = document.getElementById('ScrollList');
+    ScrollList.innerHTML = ImageBuf;
+	
+}
+var clicknum = 0;
+function ScrollClick(){
+	
+	if(clicknum == 0){
+		ScrollToon();
+		document.getElementById("button_left_on").style.display = "none";
+		document.getElementById("button_right_on").style.display = "none";
+		document.getElementById("imagePane").style.display = "none";
+		document.getElementById("ScrollList").style.display = "block";
+		document.getElementsByClassName("nice-select")[0].style.display = "none";
+		document.getElementById("comic_img").style.display = "block";
+		document.getElementById("scroll_img").style.display = "none";
+		clicknum = 1;
+	}
+	else{
+		document.getElementById("button_left_on").style.display = "block";
+		document.getElementById("button_right_on").style.display = "block";
+		document.getElementById("imagePane").style.display = "block";
+		document.getElementById("ScrollList").style.display = "none";
+		document.getElementsByClassName("nice-select")[0].style.display = "block";
+		document.getElementById("comic_img").style.display = "none";
+		document.getElementById("scroll_img").style.display = "block";
+		clicknum = 0;
+	}
+}
+
+function onZoomChange(sel){
+	var ratio = sel.options[sel.selectedIndex].value;
+	if(ratio != '') {
+		zoomRatio = ratio;
+		Redraw();
+	}
+}
+
+var listIsShown = false;
+
+function RedrawList()
+{
+	var stringBuf = '';
+	for( var i=0; i<imageCount; i++ ){
+		var listactive = '';
+		if(i == currentImage) listactive = ' class="active"';
+		stringBuf += '<li'+listactive+'><a href="#'+i+'" onClick="onClickListItem('+i+')">' + nameArray[i] + '</a></li>\n';
+	}
+
+	var fileList = document.getElementById('fileList');
+	fileList.innerHTML = '<ul class="list-vertical">'+stringBuf+'</ul>';
+}
+
+//key control enable
+window.document.onkeydown = function(e) {
+	if(!e){ e = event; }
+	//before page
+	if(e.keyCode == 37 && currentImage>0) {
+		onClickPrev();
+		location.href="#"+Number(window.location.hash.substr(1));
+	}
+	if(e.keyCode == 39 && currentImage<imageCount-1) {
+		onClickNext();
+	}
+}
---
+++ album/jquery.adv.js
@@ -0,0 +1,29 @@
+jQuery(function($){
+	var list_toggle = 0;
+	
+	$("#fileList").height($(window).height());
+	$(".control_page").height($(window).height()-40);
+	
+	$(window).resize(function(){
+		$("#fileList").height($(window).height());
+		$(".control_page").height($(".main").height()-4);
+	});
+	
+	$("#ScrollList").click(function(){
+		$("#header").animate({opacity: 'toggle'}, 100, 'swing');
+		$("#footer").animate({opacity: 'toggle'}, 100, 'swing');
+	});
+	$("#imagePane").click(function(){
+		$("#header").animate({opacity: 'toggle'}, 100, 'swing');
+		$("#footer").animate({opacity: 'toggle'}, 100, 'swing');
+	});
+	
+	$("a.header_togglelistview").click(function(){
+		$("#fileList").animate({width: 'toggle'}, 200, 'swing');
+		if(list_toggle == 1){ $("#wrap").css('margin', '0'); list_toggle = 0; }
+		else {
+			$("#wrap").css("margin", "0 0 0 220px");
+			list_toggle = 1;
+		}
+	});
+});
